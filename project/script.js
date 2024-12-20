a2b67edc55c4c4163d700531d178988e2f7483d9
s0lnc3v <sqdbll@gmail.com>
2024-12-21 02:55:21 +0300

    Update script.js

--- project/script.js
+++ project/script.js
@@ -1,34 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-    $('.clients-gallery__slider').slick({
-        slidesToShow: 6,     // показывать 6 изображений в ряду
-        slidesToScroll: 6,   // прокручивать 6 изображений
-        infinite: false,     // бесконечное прокручивание
-        autoplay: true,       // включить автопрокрутку
-        autoplaySpeed: 2000,  // скорость автопрокрутки (2 секунды)
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 3, // на маленьких экранах показывать 3 изображения
-                    slidesToScroll: 3,
-                }
-            }
-        ]
-    });
-});
-
-
-document.addEventListener("DOMContentLoaded", () => {
+$(document).ready(function () {
     const menuButton = document.querySelector(".menu-btn");
     const navMenu = document.querySelector(".nav");
     const dropdownToggles = document.querySelectorAll(".has-dropdown > a");
 
-    // Переключение основного меню
     menuButton.addEventListener("click", () => {
         navMenu.classList.toggle("active");
     });
 
-    // Переключение мини-меню
     dropdownToggles.forEach((toggle) => {
         toggle.addEventListener("click", (e) => {
             e.preventDefault();
@@ -44,73 +22,117 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         });
     });
-});
+
+    const reviewsItems = document.querySelectorAll('.reviews__item');
+    const leftArrow = document.querySelector('.reviews__arrow--left');
+    const rightArrow = document.querySelector('.reviews__arrow--right');
+    const indexSpan = document.querySelector('.reviews__index span:first-child');
+
+    let currentReview = 0;
+    document.querySelectorAll('a[href="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault(); 
+        });
+    });
+
+    function updateActiveReview(direction) {
+        const currentItem = reviewsItems[currentReview];
+        const nextReview =
+            direction === 'right'
+                ? (currentReview + 1) % reviewsItems.length
+                : (currentReview - 1 + reviewsItems.length) % reviewsItems.length;
+        const nextItem = reviewsItems[nextReview];
+
+        currentItem.classList.remove('active');
+        currentItem.classList.add(
+            direction === 'right'
+                ? 'reviews__item--slide-left-out'
+                : 'reviews__item--slide-right-out'
+        );
+
+        nextItem.style.display = 'flex';
+        nextItem.classList.add(
+            direction === 'right'
+                ? 'reviews__item--slide-left-in'
+                : 'reviews__item--slide-right-in'
+        );
+
+        setTimeout(() => {
+            currentItem.style.display = 'none';
+            currentItem.classList.remove(
+                'reviews__item--slide-left-out',
+                'reviews__item--slide-right-out'
+            );
+
+            nextItem.classList.remove(
+                'reviews__item--slide-left-in',
+                'reviews__item--slide-right-in'
+            );
+            nextItem.classList.add('active');
+            currentReview = nextReview;
+
+            indexSpan.textContent = String(currentReview + 1).padStart(2, '0');
+        }, 100);
+    }
+    leftArrow.addEventListener('click', () => updateActiveReview('left'));
+    rightArrow.addEventListener('click', () => updateActiveReview('right'));
+
+    reviewsItems[currentReview].classList.add('active');
+    reviewsItems[currentReview].style.display = 'flex';
 
 
-// Переключение меню для мобильных устройств
+    $('.clients-gallery__row--autoplay').slick({
+        slidesToShow: 6, 
+        slidesToScroll: 1, 
+        autoplay: true, 
+        autoplaySpeed: 500, 
+        infinite: true, 
+        arrows: false, 
+        dots: true, 
+        responsive: [
+            {
+                breakpoint: 768, 
+                settings: {
+                    slidesToShow: 3, 
+                }
+            },
+            {
+                breakpoint: 480, 
+                settings: {
+                    slidesToShow: 1, 
+                }
+            },
+        ],
+    });
 
-const reviewsItems = document.querySelectorAll('.reviews__item');
-const leftArrow = document.querySelector('.reviews__arrow--left');
-const rightArrow = document.querySelector('.reviews__arrow--right');
-const indexSpan = document.querySelector('.reviews__index span:first-child');
+    $('.clients-gallery__row--static').slick({
+        slidesToShow: 6, 
+        slidesToScroll: 1, 
+        autoplay: false, 
+        infinite: true, 
+        arrows: false, 
+        dots: true, 
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ],
+    });
 
-let currentReview = 0;
-document.querySelectorAll('a[href="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault(); // Предотвращаем стандартное поведение ссылки
+    const faqQuestions = document.querySelectorAll('.faq-question');
+    faqQuestions.forEach(question => {
+        question.addEventListener('click', () => {
+            const faqItem = question.closest('.faq-item');
+            faqItem.classList.toggle('active');
+        });
     });
 });
-
-// Функция для переключения отзывов
-function updateActiveReview(direction) {
-    const currentItem = reviewsItems[currentReview];
-    const nextReview =
-        direction === 'right'
-            ? (currentReview + 1) % reviewsItems.length
-            : (currentReview - 1 + reviewsItems.length) % reviewsItems.length;
-    const nextItem = reviewsItems[nextReview];
-
-    // Убираем активный отзыв
-    currentItem.classList.remove('active');
-    currentItem.classList.add(
-        direction === 'right'
-            ? 'reviews__item--slide-left-out'
-            : 'reviews__item--slide-right-out'
-    );
-
-    // Показываем следующий отзыв
-    nextItem.style.display = 'flex';
-    nextItem.classList.add(
-        direction === 'right'
-            ? 'reviews__item--slide-left-in'
-            : 'reviews__item--slide-right-in'
-    );
-
-    // Завершаем анимацию
-    setTimeout(() => {
-        currentItem.style.display = 'none';
-        currentItem.classList.remove(
-            'reviews__item--slide-left-out',
-            'reviews__item--slide-right-out'
-        );
-
-        nextItem.classList.remove(
-            'reviews__item--slide-left-in',
-            'reviews__item--slide-right-in'
-        );
-        nextItem.classList.add('active');
-        currentReview = nextReview;
-
-        // Обновляем номер отзыва
-        indexSpan.textContent = String(currentReview + 1).padStart(2, '0');
-    }, 100); // Задержка равна длительности анимации
-}
-
-// Обработчики стрелок
-leftArrow.addEventListener('click', () => updateActiveReview('left'));
-rightArrow.addEventListener('click', () => updateActiveReview('right'));
-
-// Инициализация
-reviewsItems[currentReview].classList.add('active');
-reviewsItems[currentReview].style.display = 'flex';
-
